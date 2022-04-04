ad1b4271fb8c6bec0c7b66ba5c0eedec26445f9e
Florian Wagner <slvwagner@gmail.com>
2022-04-04 10:17:04 +0200

    load and resize image

--- Installation opencv/Installation opencv.cpp
+++ Installation opencv/Installation opencv.cpp
@@ -2,10 +2,13 @@
 */
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
 int main()
 {
     printf("Hello world\n");
-
 
     // Read an image.
     cv::Mat image = cv::imread("G:/Meine Ablage/001_wagnius GmbH/100_Marketing/home page data/Wagnius DE.jpg");
@@ -14,6 +17,8 @@ int main()
         system("PAUSE");
         return EXIT_FAILURE;
     }
+    cv::resize(image, image, { 1000,int(1000/1.777778) }, 0, 0, cv::INTER_LINEAR);
+
     // Create image window named "My Image".  (You actually don't have to do
     // this step, but this command allows you to set properties of the window,
     // such as its location, or whether you can resize it.)
